bba33ccbd1883c4dff7a3a910a7ac94d7c1f3d10
Jędrzej Hasiura <hyphen133@gmail.com>
2019-12-13 20:37:27 +0100

    Added new search options to PluginSelector

--- plugins_management/plugin_selector.py
+++ plugins_management/plugin_selector.py
@@ -1,4 +1,6 @@
 from plugins_management.plugin_repository import get_plugin_repository
+from visualization_core.interfaces.VisualizationTechnique import NonGraphVisualizationTechnique, \
+    GraphVisualizationTechnique
 
 
 class PluginSelector():
@@ -10,3 +12,23 @@ class PluginSelector():
     def get_selected_plugins(name_list):
         repository = get_plugin_repository()
         return [repository.get_plugin(name) for name in name_list]
+
+    @staticmethod
+    def get_all_graph_visualization_plugins():
+        return [plugin.name for plugin in get_plugin_repository().get_all_plugins() if issubclass(plugin.__class__, GraphVisualizationTechnique)]
+
+    @staticmethod
+    def get_all_non_graph_visualization_plugins():
+        return [plugin.name for plugin in get_plugin_repository().get_all_plugins() if issubclass(plugin.__class__, NonGraphVisualizationTechnique)]
+
+    @staticmethod
+    def get_only_selected_graph_plugins_names(name_list):
+        repository = get_plugin_repository()
+        graph_plugin_names = [plugin.name for plugin in PluginSelector.get_all_graph_visualization_plugins()]
+        return [repository.get_plugin(name) for name in name_list if repository.get_plugin(name) in graph_plugin_names]
+
+    @staticmethod
+    def get_only_selected_nongraph_plugins_names(name_list):
+        repository = get_plugin_repository()
+        nongraph_plugin_names = [plugin.name for plugin in PluginSelector.all_non_graph_visualization_plugins()]
+        return [repository.get_plugin(name) for name in name_list if repository.get_plugin(name) in nongraph_plugin_names]
